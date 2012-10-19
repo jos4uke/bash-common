bd7a09c17e20ea4eee7558c2201d542d50b96507
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2012-10-19 18:25:34 +0200

    add new function to remove reads, after MAPQ filtering, with more than x independent events (XM+XO>x) and corresponding test and resources

--- pipeline_test.sh
+++ pipeline_test.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+#
+# PIPELINE TESTS SUITE
+#
 
-# Tests
-
+#---------------------
+# testHashTableOutput 
+#
 testHashTableOutput()
 {
-    PIPELINE_DEFAULT_CONFIG="/projects/ARABIDOPSIS/SCRIPTS/PIPELINE/pipeline_default.config"
     declare -A PARAMETERS_TABLE
     get_pipeline_default_parameters $PIPELINE_DEFAULT_CONFIG
     for i in "${!PARAMETERS_TABLE[@]}"
@@ -17,28 +20,81 @@ testHashTableOutput()
     assertTrue 'Unexpected void Hash table' "[ ${#PARAMETERS_TABLE[@]} -ge 1 ]"
 }
 
-
+#--------------------
+# testPrintHashTable 
+#
 testPrintHashTable()
 {
-    PIPELINE_DEFAULT_CONFIG="/projects/ARABIDOPSIS/SCRIPTS/PIPELINE/pipeline_default.config"
+    declare -A PARAMETERS_TABLE
     get_pipeline_default_parameters $PIPELINE_DEFAULT_CONFIG
 }
 
+#---------------------------------------
+# testFailedEquivalentXMXOtagsLineCount 
+#
+testFailedEquivalentXMXOtagsLineCount()
+{
+    XMtags=$(cat "${TEST_SAM}" | grep "XM:i:" | wc -l)
+    XOtags=$(cat "${TEST_SAM}" | grep "XO:i:" | wc -l)
+
+    echo -e "total reads count: $(cat $TEST_SAM | wc -l)"
+    echo -e "XM tags count: ${XMtags}"
+    echo -e "XO tags count: ${XOtags}"
+
+    assertTrue 'unexpected XM and XO tags line counts difference, should be equal' "[ ${XMtags} -eq ${XOtags} ]"
+}
+
+#-------------------------------------------------------
+# testFailedRemovingReadsWithMoreThanXIndependentEvents
+#
+testFailedRemovingReadsWithMoreThanXIndependentEvents()
+{
+    declare -A PARAMETERS_TABLE
+    get_pipeline_default_parameters $PIPELINE_DEFAULT_CONFIG
+
+    OUT=${TEST_OUTPUT_DIR}/test_mapped_MAPQ_XIE.sam
+    remove_reads_with_more_than_x_independent_events "${TEST_SAM}" >${OUT} 2>${stderrF}
+
+    echo -e "std out output:"
+    head -4 "${OUT}"
+    echo -e "std err output:" 
+    head -4 "${stderrF}"
+
+    assertTrue 'expected output to stdout' "[ -s ${OUT} ]"
+    assertFalse 'unexpected output to stderr' "[ -s ${stderrF} ]"
+
+    if [[ ${PARAMETERS_TABLE["nb_of_independent_event"]} -eq 2 ]]; then
+    assertTrue 'unexpected count for XM=2 && XO=0, should be equal 41' "[ $(cat ${OUT} | grep "XM:i:[2]" | grep "XO:i:[0]" | wc -l) -eq 41 ]"
+    assertTrue 'unexpected count for XM=1 && XO=1, should be equal 13' "[ $(cat ${OUT} | grep "XM:i:[1]" | grep "XO:i:[1]" | wc -l) -eq 13 ]"
+    assertTrue 'unexpected count for XM=1 && XO=0, should be equal 168' "[ $(cat ${OUT} | grep "XM:i:[1]" | grep "XO:i:[0]" | wc -l) -eq 168 ]"
+    assertTrue 'unexpected count for XM=0 && XO=1, should be equal 21' "[ $(cat ${OUT} | grep "XM:i:[0]" | grep "XO:i:[1]" | wc -l) -eq 21 ]"
+    assertTrue 'unexpected count for XM=0 && XO=0, should be equal 1536' "[ $(cat ${OUT} | grep "XM:i:[0]" | grep "XO:i:[0]" | wc -l) -eq 1536 ]"
+    assertTrue 'unexpected count for XM=0 && XO=2, should be equal 2' "[ $(cat ${OUT} | grep "XM:i:[0]" | grep "XO:i:[2]" | wc -l) -eq 2 ]"
+    fi
+}
 
 
-
+#
 # Configuration
+#
 
 oneTimeSetUp()
 {
     . ../lib/pipeline_lib.inc
     OUTPUT_DIR="${SHUNIT_TMPDIR}/OUTPUT"
+    stdoutF="${OUTPUT_DIR}/stdoutF"
+    stderrF="${OUTPUT_DIR}/stderrF"
     mkdir $OUTPUT_DIR
+    TEST_OUTPUT_DIR="output"
+
+    PIPELINE_DEFAULT_CONFIG="/projects/ARABIDOPSIS/SCRIPTS/PIPELINE/pipeline_default.config"
+    TEST_SAM="data/test_mapped_MAPQ.sam"
 }
 
-TearDown()
+tearDown()
 {
     rm -rf $OUTPUTDIR
+    echo "------------"
 }
 
 . /usr/local/share/shunit2-2.1.6/src/shunit2
